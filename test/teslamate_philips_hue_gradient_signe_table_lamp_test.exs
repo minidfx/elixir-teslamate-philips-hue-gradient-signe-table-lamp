ef8ba0fb0f1831b0c864a5427c9520d1ef78bbdb
Burgy Benjamin <benjamin@roomz.io>
2024-05-19 22:03:37 +0200

    Add tests for the states module.

--- test/teslamate_philips_hue_gradient_signe_table_lamp_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TeslamatePhilipsHueGradientSigneTableLampTest do
-  use ExUnit.Case
-  doctest TeslamatePhilipsHueGradientSigneTableLamp
-
-  test "greets the world" do
-    assert TeslamatePhilipsHueGradientSigneTableLamp.hello() == :world
-  end
-end
