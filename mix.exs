925e0986a6ea3f482a2074c0157b79f8188af502
Burgy Benjamin <benjamin@roomz.io>
2024-08-04 21:35:45 +0200

    Bump versions.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TeslamatePhilipsHueGradientSigneTableLamp.MixProject do
   def project do
     [
       app: :teslamate_philips_hue_gradient_signe_table_lamp,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
